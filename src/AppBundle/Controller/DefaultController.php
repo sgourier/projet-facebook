e2f7581485768bf17adc7902951cfda7a9f66933
Christophe Lugand <christophe@lugand.fr>
2016-01-12 18:30:53 +0100

    Page index
    Création page index et petit modif

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,30 +8,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-	    $session = $this->get('session');
-	    $userToken = $session->get('facebook_access_token', null);
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function indexAction(Request $request)
+	{
+		$session = $this->get('session');
+		$userToken = $session->get('facebook_access_token', null);
 
-	    if($userToken == null )
-	    {
-		    return $this->redirect($this->generateUrl('login'));
-	    }
-	    else if($userToken == 'error')
-	    {
-		    return $this->render('default/index.html.twig', array(
-			    'testString' => 'error'
-		    ));
-	    }
-		else
-		{
+		if ($userToken == null) {
+			return $this->redirect($this->generateUrl('login'));
+		} else if ($userToken == 'error') {
+			return $this->render('default/index.html.twig', array(
+				'testString' => 'error'
+			));
+		} else {
 			$user = $this->container->get('facebook_service')->getFBUser($userToken);
 			return $this->render('default/index.html.twig', array(
 				'testString' => $user['name']
 			));
 		}
-    }
+	}
 }
